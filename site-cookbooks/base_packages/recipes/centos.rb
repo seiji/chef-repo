872d11694fa308155dd3358b9eda360936f3b5ce
Seiji Toyama <seijit@me.com>
2013-05-29 18:37:16 +0900

    Add basepackage:centos

---
+++ site-cookbooks/base_packages/recipes/centos.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: base_packages
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{sysstat zsh mosh tmux emacs-nox}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+%w[acpid auditd autofs avahi-daemon avahi-dnsconfd bluetooth conman cpuspeed cups dnsmasq dund gpm hidd ip6tables irda lvm2-monitor mcstrans mdmonitor multipathd pand pcscd psacct rawdevices readahead_early readahead_later restorecond saslauthd smartd wpa_supplicant ypbind yum-updatesd].each { |name|
+  service name do
+    ignore_failure true
+    action [:disable, :stop]
+  end
+}

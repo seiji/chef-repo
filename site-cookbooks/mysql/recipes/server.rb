af19cdd32b0652520a9ff9e9cae9e0b8cd4fea40
Seiji Toyama <seijit@me.com>
2013-09-01 19:08:04 +0900

    Add my.cnf utf8bm4

--- site-cookbooks/mysql/recipes/server.rb
+++ site-cookbooks/mysql/recipes/server.rb
@@ -28,11 +28,11 @@ script "Secure_Install" do
   code <<-EOL
     export Initial_PW=`head -n 1 /root/.mysql_secret |awk '{print $(NF - 0)}'`
     mysql -u root -p${Initial_PW} --connect-expired-password -e "SET PASSWORD FOR root@localhost=PASSWORD('');"
-    mysql -u root -pyour_password -e "SET PASSWORD FOR root@'127.0.0.1'=PASSWORD('');"
-    mysql -u root -pyour_password -e "DELETE FROM mysql.user WHERE User='';"
-    mysql -u root -pyour_password -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');"
-    mysql -u root -pyour_password -e "DROP DATABASE test;"
-    mysql -u root -pyour_password -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-    mysql -u root -pyour_password -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "SET PASSWORD FOR root@'127.0.0.1'=PASSWORD('');"
+    mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+    mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');"
+    mysql -u root -e "DROP DATABASE test;"
+    mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
   EOL
 end
